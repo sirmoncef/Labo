c9ce0d6c74507caab73ada56ef466638de8823ec
sirmoncef <moncefzabat@gmail.com>
2024-12-21 21:41:20 +0000

    edit hostname variable 2

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = 'azure_project.deployment' if 'hostname' in os.environ else 'azure_project.settings'
+    settings_module = 'Laboratory.deployment' if 'hostname' in os.environ else 'Laboratory.settings'
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
